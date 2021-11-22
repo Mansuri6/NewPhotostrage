9441ee604ae6cbb7860d58b05d9b789803c293be
Mansuri <muhammedmansuri6@gmail.com>
2021-11-22 14:37:16 +0200

    new repo new commit

---
+++ PhotoStorage/Scripts/app-routes.js
@@ -0,0 +1,42 @@
+﻿(function () {
+
+    angular.module("PG", ["ui.router", "ngMessages", "ngFileUpload"])
+			.config(function ($locationProvider, $stateProvider, $urlRouterProvider, $qProvider) {
+			    $qProvider.errorOnUnhandledRejections(false);
+			    $urlRouterProvider.otherwise('/login');
+
+			    $stateProvider
+
+                .state("login", {
+                    url: "/login",
+                    templateUrl: "../modules/login/login.html",
+                    controller: 'loginController'
+                })
+                .state("main", {
+                    url: "/main",
+                    templateUrl: "../modules/images/image.html",
+                    controller: 'imageController'
+                })
+                    .state("register", {
+                        url: "/register",
+                        templateUrl: "../modules/Login/Register/register.html",
+                        controller:"registerController"
+                    })
+                    .state("images", {
+                        url: "/captured_images",
+                        templateUrl: "../modules/images/imageList/imageList.html",
+                        controller: 'imageListController'
+                    })
+
+                    .state("albumShare", {
+                        url: "/shared_album",
+                        templateUrl: "../modules/images/sharedAlbum/sharedAlbum.html",
+                        controller: 'sharedAlbumController'
+                    })
+
+			    $locationProvider.hashPrefix('');
+			    //$locationProvider.html5Mode({ enabled: true, requireBase: false, rewriteLinks: true })
+			    //$locationProvider.hashPrefix('!');
+			});
+})();
+
---
+++ PhotoStorage/Scripts/services.js
@@ -0,0 +1,21 @@
+﻿
+(function () {
+    angular.module("PG")
+        .factory("UserAuthorizationFactory", ["$http", "$q", "$window", UserAuthorizationFactory]);
+
+    function UserAuthorizationFactory($http, $q, $window) {
+        var StorageData = {};
+        var url = "http://photostorageam-001-site1.gtempurl.com/";
+        var c = {
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        };
+
+        StorageData.geturl = function () {
+            return url;
+        }
+
+        return StorageData;
+    }
+})();
